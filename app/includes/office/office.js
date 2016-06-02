9cb72b1fa0ccc054d20883dffa3522081434ccdd
Anne Tsugunyan <anne.tsugunyan@gmail.com>
2016-06-03 03:13:42 +0400

    switch map, map popup, google map link

--- app/includes/office/office.js
+++ app/includes/office/office.js
@@ -3,9 +3,40 @@ var JSContext = require("../../js/common/jscontext.js");
 var MapTools = require("../../js/common/map-tools.js");
 var htmlTemplate = require("./office.html");
 var callback = function() {
-    var Profile = JSContext.getProfile();
-    MapTools.showMap(Profile.address, 'map-container');
-    // var showMap = function(address, containerID, options, markerClickHandler) {
+
+    var suboffices = JSContext.getProfile().suboffices;
+    var $viewMap = $('#view-map');
+    var currentKey = 0;
+
+    updateMap();
+
+    $('.office-tabs a[data-toggle="tab"]').off().on('shown.bs.tab', function (e) {
+        currentKey = $(e.target).data('key');
+        updateMap();
+    });
+
+    function updateMap() {
+        $viewMap.attr('href', getGoogleMap());
+        MapTools.showSwappableMap(getAddress(), 'map-container', {}, markerClickHandler);
+    }
+
+    function markerClickHandler() {
+        var marker = this;
+        if (!marker.getPopup()) {
+            var href = getGoogleMap();
+            var address = getAddress();
+            var content = '<a target="_blank" href="'+ href +'">' + address + '</a>';
+            marker.bindPopup(content).openPopup();
+        }
+    }
+
+    function getGoogleMap() {
+        return 'http://maps.google.com/?q=' + encodeURIComponent(getAddress());
+    }
+    function getAddress() {
+        var office = suboffices[currentKey];
+        return office.street + ', ' + office.city + ' ' + office.state + ', ' + office.zip_code;
+    }
 };
 module.exports = {
     load: function() {
