91ff3d71073f8d16dae60885584a376356ff1405
Anne Tsugunyan <anne.tsugunyan@gmail.com>
2016-06-21 03:07:50 +0400

    "About me" page's  map, some tablet and mobile fixes

--- app/includes/about_me/about_me.js
+++ app/includes/about_me/about_me.js
@@ -1,8 +1,41 @@
 var Module = require("theme_module_loader");
 var JSContext = require("../../js/common/jscontext.js");
+var MapTools = require("../../js/common/map-tools.js");
 var htmlTemplate = require("./about_me.html");
 var callback = function() {
-    var Profile = JSContext.getProfile();
+    var suboffices = JSContext.getProfile().suboffices;
+    var $viewMap = $('#view-map');
+    var currentKey = 0;
+
+    updateMap();
+
+    $('.office-tabs a[data-toggle="tab"]').off().on('shown.bs.tab', function (e) {
+        currentKey = $(e.target).data('key');
+        updateMap();
+    });
+
+    function updateMap() {
+        $viewMap.attr('href', getGoogleMap());
+        MapTools.showSwappableMap(getAddress(), 'map-container', {}, markerClickHandler);
+    }
+
+    function markerClickHandler() {
+        var marker = this;
+        if (!marker.getPopup()) {
+            var href = getGoogleMap();
+            var address = getAddress();
+            var content = '<a target="_blank" href="'+ href +'">' + address + '</a>';
+            marker.bindPopup(content).openPopup();
+        }
+    }
+
+    function getGoogleMap() {
+        return 'http://maps.google.com/?q=' + encodeURIComponent(getAddress());
+    }
+    function getAddress() {
+        var office = suboffices[currentKey];
+        return office.street + ', ' + office.city + ' ' + office.state + ', ' + office.zip_code;
+    }
 };
 module.exports = {
     load: function() {
